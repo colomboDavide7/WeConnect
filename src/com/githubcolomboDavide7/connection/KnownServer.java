5cae471de09ec035cd2b9fc4d37f608ef36fbe60
Davide Colombo <davide.colombo02@universitadipavia.it>
2020-11-26 08:08:16 +0100

    Added KnownServer enum

---
+++ src/com/githubcolomboDavide7/connection/KnownServer.java
@@ -0,0 +1,25 @@
+package com.githubcolomboDavide7.connection;
+
+public enum KnownServer {
+
+    DHCP("127.0.0.4", 5000),
+    PROXY("127.0.0.5", 4000),
+    FIREWALL("127.0.0.3", 3000),
+    APP_SERVER("127.0.0.1", 7000);
+
+    public final String IPAddress;
+    public final int portNumber;
+
+    KnownServer(String IPAddress, int portNumber){
+        this.IPAddress = IPAddress;
+        this.portNumber = portNumber;
+    }
+
+    public static KnownServer get(String target){
+        for(KnownServer s : KnownServer.values())
+            if(target.equalsIgnoreCase(s.name()))
+                return s;
+        throw new IllegalArgumentException("No server that correspond to " + target);
+    }
+
+}
--- test/ConnectionTest/ConnectionTest.java
+++ test/ConnectionTest/ConnectionTest.java
@@ -2,6 +2,7 @@ package ConnectionTest;
 
 import com.githubcolomboDavide7.connection.AbstractServerConnection;
 import com.githubcolomboDavide7.connection.ConnectionFactory;
+import com.githubcolomboDavide7.connection.KnownServer;
 import org.junit.Test;
 
 import java.net.ConnectException;
@@ -16,6 +17,28 @@ public class ConnectionTest {
     private final int APP_PORT_NUM = 7000;
     private final int APP_MAX_HOST_SUPPORTED = 3;
     private final String APP_IP_ADDRESS = "127.0.0.1";
+
+    @Test
+    public void shouldTestKnownServersConnectionParameters(){
+        System.out.println("* Connection test: shouldTestKnownServersConnectionParameters()\n");
+
+        // APPLICATION SERVER
+        KnownServer appServer = KnownServer.get("app_server");
+        assertEquals(7000, appServer.portNumber);
+        assertEquals("127.0.0.1", appServer.IPAddress);
+        // DYNAMIC HOST CONFIGURATION PROTOCOL
+        KnownServer dhcp = KnownServer.get("dhcp");
+        assertEquals(5000, dhcp.portNumber);
+        assertEquals("127.0.0.4", dhcp.IPAddress);
+        // PROXY
+        KnownServer proxy = KnownServer.get("proxy");
+        assertEquals(4000, proxy.portNumber);
+        assertEquals("127.0.0.5", proxy.IPAddress);
+        // FIREWALL
+        KnownServer inferno = KnownServer.get("firewall");
+        assertEquals(3000, inferno.portNumber);
+        assertEquals("127.0.0.3", inferno.IPAddress);
+    }
 
     @Test
     public void shouldReturnDynamicHostConfigConnection(){
@@ -45,6 +68,5 @@ public class ConnectionTest {
             fail();
         }
     }
-
-
+    
 }
