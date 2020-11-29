8ad93eb536bc2da040721c2c4208d8b89a095b32
Davide Colombo <davide.colombo02@universitadipavia.it>
2020-11-29 19:44:23 +0100

    Added client test for connection

--- test/clientTest/ClientTest.java
+++ test/clientTest/ClientTest.java
@@ -6,6 +6,8 @@ import com.githubcolomboDavide7.connection.*;
 import com.githubcolomboDavide7.servers.*;
 
 import org.junit.*;
+import sun.jvm.hotspot.oops.InstanceClassLoaderKlass;
+
 import static org.junit.Assert.*;
 
 public class ClientTest {
@@ -35,5 +37,22 @@ public class ClientTest {
             fail();
         }
     }
+
+    @Test(expected = ConnectException.class)
+    public void shouldThrowConnectExceptionWhenTryingToCloseAndNotConnected() throws ConnectException {
+        System.out.println("* Client test: shouldThrowConnectExceptionWhenTryingToCloseAndNotConnected()\n");
+        IClient c = new Client();
+        c.close(KnownServer.APP_SERVER);
+    }
+
+    @Test(expected = ConnectException.class)
+    public void shouldThrowConnectExceptionWhenTryingToOpenAnAlreadyExistingConnection() throws ConnectException {
+        System.out.println("* Client test: shouldThrowConnectExceptionWhenTryingToOpenAnAlreadyExistingConnection()\n");
+        IClient c = new Client();
+        c.open(KnownServer.APP_SERVER);
+        assertTrue(true);       // to assure we are right connected the first time
+        c.open(KnownServer.APP_SERVER);
+    }
+    
 
 }
